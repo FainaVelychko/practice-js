3c2248cb30a94525f40905a607b5a797c493e789
FainaVelychko <fainarw@gmail.com>
2024-02-22 23:17:22 +0200

    task next

--- js/module-5.js
+++ js/module-5.js
@@ -1,11 +1,25 @@
-function calculateTotalPrice(orderedItems) {
-  let totalPrice = 0;
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
 
-  orderedItems.forEach(function (number, index) {
-    totalPrice += number;
+//   orderedItems.forEach(function (number, index) {
+//     totalPrice += number;
+//   });
+
+//   return totalPrice;
+// }
+
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
+
+const changeEven = (numbers, value) => {
+  let newArrey = [];
+  numbers.forEach(element => {
+    if (element % 2 === 0) {
+      newArrey.push(element + value);
+    } else {
+      newArrey.push(element);
+    }
   });
+  return newArrey;
+};
 
-  return totalPrice;
-}
-
-console.log(calculateTotalPrice([12, 85, 37, 4]));
+console.log(changeEven([1, 2, 3, 4, 5], 10));
